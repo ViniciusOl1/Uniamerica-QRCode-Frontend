efb088d5f6924087119d3fd18f9b50cb08580cda
Vinicius Oliveira de Freitas <vinicius.vof@outlook.com>
2020-05-26 10:55:36 -0300

    Responsividade

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,5 +1,7 @@
 import React, { useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
 import './index.css';
 
 import api from '../../services/api';
@@ -21,14 +23,27 @@ function Header() {
     localStorage.clear();
     history.push('/');
   }
+  function handleMenu() {
+    let rMenu = document.getElementById("topbar");
+    if (rMenu.className === "topbar") {
+      return rMenu.className += " responsive";
+    } else {
+      return rMenu.className = "topbar";
+    }
+  }
 
 
   return (
-    <header>
+    <header id="topbar" className="topbar">
       <img src={logoImg} alt="Uniamérica - QRCode" />
 
       <nav>
         <ul>
+          <li class="responsive-menu-icon">
+            <a href="javascript:void(0);" onClick={handleMenu}>
+              <FontAwesomeIcon icon={faBars} />
+            </a>
+          </li>
           <li><a href="/dashboard">Dashboard</a></li>
           <li><a href="/alunos">Alunos</a></li>
           <li><a href="/professores">Professores</a></li>
